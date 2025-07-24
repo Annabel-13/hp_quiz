331c241111f228fffa2e859949590c744b605eab
matcha_frontend <AnnabelFeer@gmail.com>
2025-07-24 16:12:56 +0200

    deleted UseEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { fetchQuizQuestions, QuestionState } from './API';
 import { GlobalStyle, Wrapper } from './App.styles';
 import QuestionCard from './components/QuestionCard';
